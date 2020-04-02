43e71495fee9bf48320466464237829d35ac8624
Clumpy Tuna <anuhinm@mail.ru>
2020-04-02 22:11:48 +0300

    Add BS

---
+++ homework/practice-2/binary-search.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <vector>
+
+
+// Implement Binary Search using this function template
+bool BinarySearch(const std::vector<int>& v) {
+  // Put your code here
+}
+
+
+int main() {
+
+  return 0;
+}
