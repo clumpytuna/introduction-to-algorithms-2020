015cb238b6eb87a134a4a1b02ffb2a0a29d26b88
Clumpy Tuna <anuhinm@mail.ru>
2020-04-03 21:45:01 +0300

    Update type

--- homework/practice-2/binary-search.cpp
+++ homework/practice-2/binary-search.cpp
@@ -5,7 +5,7 @@
 
 // Implement Binary Search using this function template
 // return -1 if there is no such element, return an index if element exist
-int BinarySearch(const std::vector<int>& v, long element) {
+int BinarySearch(const std::vector<long>& v, long element) {
   // Put your code here
 }
 
