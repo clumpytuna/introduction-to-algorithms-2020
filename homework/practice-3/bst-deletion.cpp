f34778175ff61843c9b9590262baa4a2b04789e3
Clumpy Tuna <anuhinm@mail.ru>
2020-05-13 01:08:07 +0300

    Update a description

--- homework/practice-3/bst-deletion.cpp
+++ homework/practice-3/bst-deletion.cpp
@@ -1,11 +1,20 @@
-// Implement Insert and Lookup methods of Binary Search Tree. You have to follow the template:
+// Implement Insert and Delete methods of Binary Search Tree. You have to follow the template:
 
 // The first line of input contains one integer number N (1 < N < 1000),
 // that denote the number of elements in the tree.
-// The second line contains N integer numbers (1 < number < 10 000).
-// The third line contains one integer number M (1 < M < 1000).
 
-// If M number exists in the tree print "YES", if not print "NO".
+// The second line contains N integer numbers (1 < number < 10 000,
+// numbers you need to insert in the tree.
+
+// The third line contains one integer number M (1 < M < 10 000),
+// that denote number of elements you need to delete from the tree
+
+// The fourth line contains M integer numbers (1 < number < 10 000),
+// numbers you need to delete from the tree.
+
+// The fifths line contains one integer number X (1 < X < 1000).
+
+// If X number exists in the tree print "YES", if not print "NO".
 
 class BinarySearchTree {
  public:
